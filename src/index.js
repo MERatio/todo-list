8657a4e6a4c163d32682162d8964dcc18ef704b6
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-11-05 18:50:06 +0800

    Hide Add Todo btn if there's no projects at launch

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	} else {
 		addInitialElements();
 	}
+	dom.determineAddTodoDisplay();
 	dom.addFormValidations();
 	dom.addEventListeners();
 });
